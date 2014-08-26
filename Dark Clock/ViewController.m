893f92d099e3434ea87db575683a8b458bc8ae1f
Justin Houck <jhouck@itriagehealth.com>
2014-08-26 12:57:52 -0600

    added recursion

--- Dark Clock/ViewController.m
+++ Dark Clock/ViewController.m
@@ -9,6 +9,12 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+	NSDateFormatter *dateFormatter;
+
+}
+@property (weak, nonatomic) IBOutlet UILabel *uiClock;
+
 
 @end
 
@@ -16,6 +22,12 @@
             
 - (void)viewDidLoad {
 	[super viewDidLoad];
+	dateFormatter = [[NSDateFormatter alloc] init];
+	[dateFormatter setDateFormat: @"MM-dd-yyyy\n hh:mm:ss a"];
+ 
+	[self updateClock];
+
+	
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,5 +35,15 @@
 	[super didReceiveMemoryWarning];
 	// Dispose of any resources that can be recreated.
 }
+
+- (void)updateClock {
+	self.uiClock.text = [dateFormatter stringFromDate:[NSDate date]];
+	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+		self.uiClock.text = [dateFormatter stringFromDate:[NSDate date]];
+		[self updateClock];
+	});
+
+}
+
 
 @end
